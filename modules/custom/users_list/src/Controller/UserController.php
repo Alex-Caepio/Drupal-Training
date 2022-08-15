5e3a76b6f5745ce5c8692ef0e642d1f0a85d836a
a.brutov <a.brutov@andersenlab.com>
2022-08-15 18:04:40 +0300

    Added possibility to download excel file containing site's users

--- modules/custom/users_list/src/Controller/UserController.php
+++ modules/custom/users_list/src/Controller/UserController.php
@@ -2,22 +2,13 @@
 
 namespace Drupal\users_list\Controller;
 
-use Drupal\users_list\service\PhpSpreadSheetService;
 
 class UserController
 {
     protected $spreadSheetService;
 
-
-
-//    public function __construct(PhpSpreadSheetService $spreadSheetService)
-//    {
-//      $this->spreadSheetService = $spreadSheetService;
-//    }
     public function list_of_users()
     {
-//      $barista = \Drupal::getContainer()
-//        ->get(PhpSpreadSheetService::class);
       $class = \Drupal::service('users_list.php_spread_sheet_service');
       return $class->spreadSheet();
     }
